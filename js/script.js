8ab8d0a74c836bc87c3e38b83cdf24c2feafb526
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-10 15:33:19 +0100

    Adds Email's array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('JS OK');
+/* # Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere
+Stampa un messaggio appropriato in console sull’esito del controllo. */
+
+const savedEmails = ['primaemail@gmail.com', 'secondaemail@gmail.com', 'emailacaso@gmail.com', 'testemail@gmail.com'];
