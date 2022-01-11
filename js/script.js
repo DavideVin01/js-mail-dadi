fe87d9dbeacabd81ca3fbd15452227410e007685
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-11 15:02:45 +0100

    Fixes mail exercise

--- js/script.js
+++ js/script.js
@@ -4,24 +4,26 @@ Chiedi all’utente la sua email.
 controlla che sia nella lista di chi può accedere
 Stampa un messaggio appropriato in console sull’esito del controllo. */
 
-const savedEmails = ['primaemail@gmail.com', 'secondaemail@gmail.com', 'emailacaso@gmail.com', 'testemail@gmail.com'];
+const displayElement = document.getElementById('display');
+
+const savedEmails = ['davide@gmail.com', 'marcello@gmail.com', 'beatrice@gmail.com', 'carmine@gmail.com'];
+console.table(savedEmails);
 
 const userEmail = prompt('Inserisci qui la tua email');
 console.log(userEmail);
 
-// # First case > Not working
-/* for (let i = 0; i < savedEmails.length; i++) {
-    if (userEmail === savedEmails) {
-        console.log(`Benvenuto ${userEmail}`);
-    } else {
-        console.log('La tua mail non risulta registrata.');
-    }
-} */
+let isSaved = false;
 
-// # Second case > Working
-const mailValidation = savedEmails.find(value => value === userEmail);
-if (mailValidation) {
-    console.log(`Benvenuto ${userEmail}`);
+for (let i = 0; i < savedEmails.length; i++) {
+    if (userEmail === savedEmails[i]) {
+        console.log(`Email riconosciuta.`);
+        isSaved = true;
+    }
+}
+
+if (isSaved) {
+    displayElement.innerText = `Benvenuto ${userEmail}`;
 } else {
-    console.log('La tua mail non risulta registrata.');
+    displayElement.innerText = `Spiacente, la tua email non risulta registrata.`;
+    console.log('Email errata.');
 }
